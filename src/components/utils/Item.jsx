21a9f36fa4c3b5b04b1c63c5efbe1cf6f10f630f
moolasubbaramireddy <subbaramireddym@gmail.com>
2023-02-05 20:48:20 +0530

    adding

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,22 +1,37 @@
-import { StarIcon } from '@heroicons/react/24/solid'
-import React from 'react'
+import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
-const Item = ({id,title,text,rating,btn,color,img,price,shadow}) => {
-  console.log(id)
+const Item = ({ id, title, text, rating, btn, color, img, price, shadow }) => {
+  //console.log(id)
   return (
     <>
-    <div>
       <div>
-        <h1>{title}</h1>
-        <h1>{text}</h1>
         <div>
-          <div><StarIcon className='icon-style text-slate-900'/><h1>{rating}</h1></div>
+          <h1>{title}</h1>
+          <h1>{text}</h1>
+          <div>
+            <div>
+              <h1>{price}</h1>
+            </div>
+            <div>
+              <StarIcon className="icon-style text-slate-900npm den" />
+              <h1>{rating}</h1>
+            </div>
+            <div>
+              <button type='button' className="" > <ShoppingBagIcon className="icon-style text-slate-900" /> </button>
+              <button type="button" className="">{btn} </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img 
+          src={img} 
+          alt='img/item-img'
+          className=""/>
         </div>
       </div>
-      <div></div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
